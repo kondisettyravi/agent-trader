3472925b045d93ed95a5a46a5892191b9fe0022e
Ravi Teja Kondisetty <kondisettyravi@gmail.com>
2025-01-06 19:44:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.title("ETRM Data Retrieval and Visualization Tool")
 # Function to execute a query and return a pandas DataFrame
 def execute_query(query):
     # Replace with your Azure Function endpoint
-    azure_function_endpoint = "YOUR_AZURE_FUNCTION_ENDPOINT"
+    azure_function_endpoint = "https://agent-trader.azurewebsites.net/api/*?"
 
     try:
         response = requests.post(
